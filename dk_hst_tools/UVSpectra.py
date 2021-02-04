641afb3eea3818313a2a6f74a6d8a794d39742b1
Dhanesh (DK) Krishnarao <dawnkeykawng08@me.com>
2021-02-04 16:30:26 -0600

    Analysis Functionality
    Add basic plotting and statistical testing capabilities

--- dk_hst_tools/UVSpectra.py
+++ dk_hst_tools/UVSpectra.py
@@ -4,6 +4,8 @@ import numpy as np
 from astropy import units as u 
 from astropy.table import Table 
 from astropy.coordinates import SkyCoord
+from astropy.table.column import (BaseColumn, Column, MaskedColumn, _auto_names, FalseArray,
+                     col_copy, _convert_sequence_data_to_array)
 
 from .uvDataMixin import UVSpectraMixin
 
@@ -12,6 +14,8 @@ import os
 import glob
 import io
 import pandas as pd
+
+from astroquery.simbad import Simbad
 
 directory = os.path.dirname(__file__)
 
@@ -24,134 +28,267 @@ class UVSpectra(UVSpectraMixin, Table):
     Parameters
     ----------
 
-    directory: `str`, 
+    directory: `str`, optional 
         Directory where data is stored, 
         organized as one folder per source/direction
     bart: `bool`, optional, must be keyword
         if True (default) assumes reading in Bart Wakker's data analysis products
+    query: `bool`, optional must be keyword
+        if True (default) will Simbad query the source names and store their information
 
     """
 
-    def __init__(self, directory, bart = True, **kwargs):
+    def __init__(self, path = None, bart = True, query = True,
+                 source_dirs = None, source_names = None, source_info = None, 
+                 LMC_info = None, LMC_coords = None, source_coords = None,
+                 coords_dict = None, abund_files = None, 
+                 raw_df = None, 
+                 raw_table = None,
+                 **kwargs):
 
-        # Read in paths for data
+        if bart:
+            # Read in paths for data
 
-        self.path = directory
-        self.source_dirs = glob.glob(os.path.join(directory,"*"))
+            self.path = path
+            if source_dirs is None:
+                self.source_dirs = glob.glob(os.path.join(path,"*"))
+            else:
+                self.source_dirs = source_dirs
 
-        # Get source names:
-        self.source_names = [path.split("/")[-1] for path in self.source_dirs]
+            # Get source names:
+            if source_names is None:
+                self.source_names = [path.split("/")[-1] for path in self.source_dirs]
+            else:
+                self.source_names = source_names
 
-        self.abund_files = glob.glob(os.path.join(self.path,"*/*ABUND.txt"))
-        # Read in abundance measurements
-        names = [
-            "SOURCE", 
-            "CLASS_FLAG", 
-            "NAME", 
-            "VELOCITY", 
-            "VMIN", 
-            "VMAX", 
-            "MEAN_VEL", 
-            "VEL_GALDEV", 
-            "MEASURE_FLAG",
-            "N_HI",
-            "N_CII", 
-            "N_OI", 
-            "N_NI", 
-            "N_AlII", 
-            "N_SiII", 
-            "N_SiIII", 
-            "N_SII", 
-            "N_PII", 
-            "N_FeII", 
-            "N_OVI",
-            "N_CIV", 
-            "N_NV", 
-            "N_SiIV", 
-            "LOG_OVI/CIV", 
-            "LOG_CIV/NV", 
-            "LOG_CIV/SiIV", 
-            "LOG_CIV/II", 
-            "LOG_SiIII/II"
-        ]
+            # Query basic info to store
+            if source_info is None:
+                self.source_info = Simbad.query_objects(np.unique(self.source_names))
+                self.source_info["SOURCE"] = self.source_names
+            else:
+                self.source_info = source_info
 
-        def read_abund_file(filename, names = names):
-            stream = os.popen('cat {} | grep " m "'.format(filename))
-            output = stream.read()
-            data = io.StringIO(output)
-            return  pd.read_csv(data, delim_whitespace=True, names = names, na_values = ".")
+            if LMC_info is None:
+                self.LMC_info = Simbad.query_object("LMC")
+            else:
+                self.LMC_info = LMC_info
+            
 
-        frames = [read_abund_file(abund_file) for abund_file in self.abund_files]
+            # Set SkyCoord objects for sources
+            if LMC_coords is None:
+                self.LMC_coords = SkyCoord(ra = self.LMC_info["RA"], 
+                    dec = self.LMC_info["DEC"], 
+                    unit = (u.hourangle, u.deg), 
+                    frame = "icrs")
+            else:
+                self.LMC_coords = LMC_coords
 
+            if source_coords is None:
+                self.source_coords = SkyCoord(ra = self.source_info["RA"], 
+                    dec = self.source_info["DEC"], 
+                    unit = (u.hourangle, u.deg),
+                    frame = "icrs")
+            else:
+                self.source_coords = source_coords
 
-        self.raw_df = pd.concat(frames)
-        self.raw_table = Table.from_pandas(self.raw_df)
+            if coords_dict is None:
+                self.coords_dict = {}
+                for key,value in zip(self.source_names,self.source_coords):
+                    self.coords_dict[key] = value
+            else:
+                self.coords_dict = coords_dict
 
-        # Assign Units and convert data when necessary
-        vel_cols = ["VELOCITY", "VMIN", "VMAX", "MEAN_VEL", "VEL_GALDEV"]
-        for key in vel_cols:
-            new_col = self.raw_table[key] * u.km/u.s
-
-            self.raw_table[key] = new_col
-
-        N_cols = [
-            "N_HI",
-            "N_CII", 
-            "N_OI", 
-            "N_NI", 
-            "N_AlII", 
-            "N_SiII", 
-            "N_SiIII", 
-            "N_SII", 
-            "N_PII", 
-            "N_FeII", 
-            "N_OVI",
-            "N_CIV", 
-            "N_NV", 
-            "N_SiIV", 
-        ]
-
-        for species in N_cols:
-            upper_limit_mask = [str(val)[0] == "<" for val in self.raw_table[species]]
-            lower_limit_mask = [str(val)[0] == "<" for val in self.raw_table[species]]
-            new_col = []
-            for val in self.raw_table[species]:
-                try:
-                    new_col.append(float(str(val).split("<")[-1].split(">")[-1]))
-                except ValueError:
-                    new_col.append(np.nan)
-            new_col = np.array(new_col)
-
-            self.raw_table[species] = 10**new_col * u.cm**-2
-            self.raw_table["{}_UPPERLIMIT".format(species)] = upper_limit_mask
-            self.raw_table["{}_LOWERLIMIT".format(species)] = upper_limit_mask
+            if abund_files is None:
+                self.abund_files = glob.glob(os.path.join(self.path,"*/*ABUND.txt"))
+            else:
+                self.abund_files = abund_files
 
 
+            # Read in abundance measurements
+            names = [
+                "SOURCE", 
+                "CLASS_FLAG", 
+                "NAME", 
+                "VELOCITY", 
+                "VMIN", 
+                "VMAX", 
+                "MEAN_VEL", 
+                "VEL_GALDEV", 
+                "MEASURE_FLAG",
+                "N_HI",
+                "N_CII", 
+                "N_OI", 
+                "N_NI", 
+                "N_AlII", 
+                "N_SiII", 
+                "N_SiIII", 
+                "N_SII", 
+                "N_PII", 
+                "N_FeII", 
+                "N_OVI",
+                "N_CIV", 
+                "N_NV", 
+                "N_SiIV", 
+                "LOG_OVI/CIV", 
+                "LOG_CIV/NV", 
+                "LOG_CIV/SiIV", 
+                "LOG_CIV/II", 
+                "LOG_SiIII/II"
+            ]
 
-        ratio_cols = [
-            "LOG_OVI/CIV", 
-            "LOG_CIV/NV", 
-            "LOG_CIV/SiIV", 
-            "LOG_CIV/II", 
-            "LOG_SiIII/II"
-        ]
+            def read_abund_file(filename, names = names):
+                stream = os.popen('cat {} | grep " m "'.format(filename))
+                output = stream.read()
+                data = io.StringIO(output)
+                return  pd.read_csv(data, delim_whitespace=True, names = names, na_values = ".")
 
-        for ratio in ratio_cols:
-            num_species, denom_species = ratio.split("/")
-            num_species = num_species.split("_")[-1]
-            if denom_species == "II":
-                denom_species = "{}II".format(num_species.split("I")[0])
-            self.raw_table[ratio] = np.log10(self.raw_table["N_{}".format(num_species)] / self.raw_table["N_{}".format(denom_species)])
-            self.raw_table["{}_UPPERLIMIT".format(ratio)] = self.raw_table["N_{}_UPPERLIMIT".format(num_species)]
-            self.raw_table["{}_LOWERLIMIT".format(ratio)] = self.raw_table["N_{}_UPPERLIMIT".format(denom_species)]
-            self.raw_table[ratio][self.raw_table["{}_UPPERLIMIT".format(ratio)] & self.raw_table["{}_LOWERLIMIT".format(ratio)]] = np.nan
-            self.raw_table["{}_UPPERLIMIT".format(ratio)] |= self.raw_table["N_{}_LOWERLIMIT".format(denom_species)]
-            self.raw_table["{}_LOWERLIMIT".format(ratio)] |= self.raw_table["N_{}_LOWERLIMIT".format(num_species)]
-
-            self.raw_table[ratio].unit = None
+            
 
 
-        super().__init__(data = self.raw_table, **kwargs)
+            if raw_df is None:
+                frames = [read_abund_file(abund_file) for abund_file in self.abund_files]
+                self.raw_df = pd.concat(frames)
+            else:
+                self.raw_df = raw_df
+            if raw_table is None:
+                self.raw_table = Table.from_pandas(self.raw_df)
+            
 
-        
+
+                # Assign Units and convert data when necessary
+                vel_cols = ["VELOCITY", "VMIN", "VMAX", "MEAN_VEL", "VEL_GALDEV"]
+                for key in vel_cols:
+                    new_col = self.raw_table[key] * u.km/u.s
+
+                    self.raw_table[key] = new_col
+
+                N_cols = [
+                    "N_HI",
+                    "N_CII", 
+                    "N_OI", 
+                    "N_NI", 
+                    "N_AlII", 
+                    "N_SiII", 
+                    "N_SiIII", 
+                    "N_SII", 
+                    "N_PII", 
+                    "N_FeII", 
+                    "N_OVI",
+                    "N_CIV", 
+                    "N_NV", 
+                    "N_SiIV", 
+                ]
+
+                for species in N_cols:
+                    upper_limit_mask = [str(val)[0] == "<" for val in self.raw_table[species]]
+                    lower_limit_mask = [str(val)[0] == ">" for val in self.raw_table[species]]
+                    new_col = []
+                    for val in self.raw_table[species]:
+                        try:
+                            new_col.append(float(str(val).split("<")[-1].split(">")[-1]))
+                        except ValueError:
+                            new_col.append(np.nan)
+                    new_col = np.array(new_col)
+
+                    self.raw_table[species] = 10**new_col * u.cm**-2
+                    self.raw_table["{}_UPPERLIMIT".format(species)] = upper_limit_mask
+                    self.raw_table["{}_LOWERLIMIT".format(species)] = lower_limit_mask
+
+
+
+                ratio_cols = [
+                    "LOG_OVI/CIV", 
+                    "LOG_CIV/NV", 
+                    "LOG_CIV/SiIV", 
+                    "LOG_CIV/II", 
+                    "LOG_SiIII/II"
+                ]
+
+                for ratio in ratio_cols:
+                    num_species, denom_species = ratio.split("/")
+                    num_species = num_species.split("_")[-1]
+                    if denom_species == "II":
+                        denom_species = "{}II".format(num_species.split("I")[0])
+                    self.raw_table[ratio] = np.log10(self.raw_table["N_{}".format(num_species)] / self.raw_table["N_{}".format(denom_species)])
+                    self.raw_table["{}_UPPERLIMIT".format(ratio)] = self.raw_table["N_{}_UPPERLIMIT".format(num_species)]
+                    self.raw_table["{}_LOWERLIMIT".format(ratio)] = self.raw_table["N_{}_UPPERLIMIT".format(denom_species)]
+                    self.raw_table[ratio][self.raw_table["{}_UPPERLIMIT".format(ratio)] & self.raw_table["{}_LOWERLIMIT".format(ratio)]] = np.nan
+                    self.raw_table["{}_UPPERLIMIT".format(ratio)] |= self.raw_table["N_{}_LOWERLIMIT".format(denom_species)]
+                    self.raw_table["{}_LOWERLIMIT".format(ratio)] |= self.raw_table["N_{}_LOWERLIMIT".format(num_species)]
+
+                    self.raw_table[ratio].unit = None
+
+            else:
+                self.raw_table = raw_table
+
+
+
+            super().__init__(data = self.raw_table, **kwargs)
+
+            # Add in coordinate information 
+            self.SkyCoords = self.get_SkyCoords()
+            self.SkyCoords_gal = self.SkyCoords.transform_to("galactic")
+
+            # Add to table
+            self["RA"] = self.SkyCoords.ra
+            self["DEC"] = self.SkyCoords.dec
+            self["GAL-LON"] = self.SkyCoords_gal.l 
+            self["GAL-LAT"] = self.SkyCoords_gal.b 
+
+            # Add impact parameters
+            self["LMC_ANG_B"] = self.get_angular_impact_parameter(self.LMC_coords, self.SkyCoords)
+            self["LMC_B"] = self.get_LMC_impact_parameter(self.SkyCoords)
+
+            # Simple Velocity cut flagging
+            self["250_PM_30KMS_FLAG"] = self["MEAN_VEL"] < 280*u.km/u.s
+            self["250_PM_30KMS_FLAG"] &= self["MEAN_VEL"] > 220*u.km/u.s
+
+        else:
+            raise NotImplementedError("Not yet implemented to handle data not processed by Bart Wakker!")
+
+    def _new_from_slice(self, slice_):
+        """Create a new table as a referenced slice from self."""
+
+        table = self.__class__(masked=self.masked, 
+                               path = self.path,
+                               source_dirs = self.source_dirs, 
+                               source_names = self.source_names, 
+                               source_info = self.source_info, 
+                               LMC_info = self.LMC_info, 
+                               LMC_coords = self.LMC_coords, 
+                               source_coords = self.source_coords,
+                               coords_dict = self.coords_dict,
+                               abund_files = self.abund_files, 
+                               raw_df = self.raw_df, 
+                               raw_table = self.raw_table)
+        if self.meta:
+            table.meta = self.meta.copy()  # Shallow copy for slice
+        table.primary_key = self.primary_key
+
+        newcols = []
+        for col in self.columns.values():
+            newcol = col[slice_]
+
+            # Note in line below, use direct attribute access to col.indices for Column
+            # instances instead of the generic col.info.indices.  This saves about 4 usec
+            # per column.
+            if (col if isinstance(col, Column) else col.info).indices:
+                # TODO : as far as I can tell the only purpose of setting _copy_indices
+                # here is to communicate that to the initial test in `slice_indices`.
+                # Why isn't that just sent as an arg to the function?
+                col.info._copy_indices = self._copy_indices
+                newcol = col.info.slice_indices(newcol, slice_, len(col))
+
+                # Don't understand why this is forcing a value on the original column.
+                # Normally col.info does not even have a _copy_indices attribute.  Tests
+                # still pass if this line is deleted.  (Each col.info attribute access
+                # is expensive).
+                col.info._copy_indices = True
+            else:
+                newcol.info.indices = []
+
+            newcols.append(newcol)
+
+        self._make_table_from_cols(table, newcols, verify=False, names=self.columns.keys())
+        return table
         
--- dk_hst_tools/uvDataMixin.py
+++ dk_hst_tools/uvDataMixin.py
@@ -1,10 +1,347 @@
 import logging
 
+import astropy.units as u
+from astropy.coordinates import SkyCoord, concatenate
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import Normalize
+
+from pymccorrelation import pymccorrelation
+
+
 
 class UVSpectraMixin(object):
-	"""
-	Mixin class with convenience functionality for HST COS Data
+    """
+    Mixin class with convenience functionality for HST COS Data
 
-	"""
+    """
 
-	
+    def get_SkyCoords(self):
+        """
+        Get SkyCoords for each entry in table
+
+        Parameters
+        ----------
+        
+        """
+
+        return concatenate(([self.coords_dict[source_name] for source_name in self["SOURCE"]]))
+
+
+
+
+    
+    def get_angular_impact_parameter(self, target_coordinate, coords):
+        """
+        Calculate impact parameter (projected angular distance from target)
+
+        Parameters
+        ----------
+        target_coordinate: `astropy.coordinates.SkyCoord`
+            Target galaxy coordinate
+        coords: `astropy.coordinates.SkyCoord`
+            list of coordinates to calculate impact parameter for
+
+
+        """
+        return target_coordinate.separation(coords)
+
+    def get_LMC_impact_parameter(self, coords, distance = 50*u.kpc):
+        """
+        Calculate impact parameter to LMC assuming some distances
+
+        Parameters
+        ----------
+        coords: `astropy.coordinates.SkyCoord`
+            list of coordinates to calculate impact parameter for
+        distance: `u.Quantity, number`, optional, must be keyword
+            assumed distance to LMC
+        """
+
+        if not hasattr(distance, "unit"):
+            distance *= u.kpc
+            logging.warning("No units provided for distance, assuming kpc!")
+
+        return np.tan(self.LMC_coords.separation(coords)) * distance
+
+    def get_LMC_mccorrelation(self, 
+                              ion, 
+                              coeff = "kendallt", 
+                              Nboot = 1000, 
+                              as_dict = True,
+                              **kwargs):
+        """
+        Calculate rank correlation coefficients using bootstrapping and censoring
+
+        Parameters
+        ----------
+        ion:  `str`
+            Name of ion
+        coeff: `str`, optional, must be keyword
+            rank coefficient to calculate
+        Nboot: `number`, optional, must be keyword
+            number of bootstrap resamples to draw 
+        as_dict: `bool`, optional ,must be keyword
+            if True, returns as labeled dictionary of entries
+        **kwargs:
+            keywords passed to pymccorrelation
+        """
+
+        x = self["LMC_B"]
+        y = self["N_{}".format(ion)]
+        ylim = np.zeros_like(x, dtype = int)
+        ylim[self["N_{}_LOWERLIMIT".format(ion)]] = -1
+        ylim[self["N_{}_UPPERLIMIT".format(ion)]] = 1
+
+        xlim = np.zeros_like(x, dtype = int)
+
+        res = pymccorrelation(x, y, xlim = xlim, ylim = ylim, 
+            coeff = coeff, Nboot = Nboot, **kwargs)
+
+        if as_dict:
+            out = {}
+            if "return_dist" in kwargs:
+                if kwargs["return_dist"]:
+                    out["coeff_percentiles"] = res[0]
+                    out["p-value_percentiles"] = res[1]
+                    out["coeff_dist"] = res[2]
+                    out["p-value_dist"] = res[3]
+                else:
+                    out["coeff_percentiles"] = res[0]
+                    out["p-value_percentiles"] = res[1]
+            else:
+                out["coeff_percentiles"] = res[0]
+                out["p-value_percentiles"] = res[1]
+
+            return out
+        else:
+            return res
+
+
+
+
+
+
+
+
+    def plot_NvB_LMC(self, ion, 
+                     ax = None, 
+                     log = False, 
+                     add_labels = False, 
+                     colorbar = False,
+                     length_factor = 0.25,
+                     head_length_factor = 0.075,
+                     limit_kwargs = {},
+                     colorbar_kwargs = {}, 
+                     **kwargs):
+        """
+        Make a plot of column density vs. impact parameter to LMC, including accounting for upper limits
+
+        Parameters
+        ----------
+
+        ion: `str`
+            name of ion to plot column density for
+        ax: `matplotlib.pyplot.axis', optional, must be keyword
+            if provided, plots on provided axis, else makes new figure and axis
+        log: `bool`, optional, must be keyword
+            if True, plots log column densities
+        add_labels: `bool`, optional, must be keyword
+            if True, adds axes labels
+        colorbar: `bool`, optional, must be keyword
+            if True, add colorbar
+        length_factor:, `number`, optional, must be keyword
+            factor to set length of limit arrows
+        head_length_factor: `number`, optional must be keyword
+            if not log, then used to set length scaling of arrow head
+        limit_kwargs: `dict`, optional, must be keyword
+            dictionary of keywords to pass to plotting limit markers
+        colorbar_kwargs: `dict`, optional, must be keyword
+            dictionary of keywords to pass to plotting of colorbar
+        **kwargs: keywords passed to ax.scatter
+        
+        Returns
+        -------
+        ax: `matplotlib.pyplot.axis`
+        """
+
+        # Make sure ion exists in data
+        if "N_{}".format(ion) not in self.keys():
+            raise ValueError("Specified ion is not in this data!")
+
+        if ax is None:
+            fig, ax = plt.subplots()
+
+        # Check limit kwargs
+        if ("color" in kwargs) & ("color" not in limit_kwargs):
+            limit_kwargs["color"] = kwargs["color"]
+        if ("lw" not in limit_kwargs):
+            limit_kwargs["lw"] = 2
+        if ("alpha" in kwargs) & ("alpha" not in limit_kwargs):
+            limit_kwargs["alpha"] = kwargs["alpha"]
+
+        if ("zorder" not in kwargs):
+            kwargs["zorder"] = 1
+        if ("zorder" in kwargs) & ("zorder" not in limit_kwargs):
+            limit_kwargs["zorder"] = kwargs["zorder"] - 1
+
+
+        if "label" not in kwargs:
+            kwargs["label"] = ion
+
+        if ("c" in kwargs) & ("norm" not in kwargs):
+            if "vmin" not in kwargs:
+                vmin = np.nanmin(kwargs["c"])
+            else:
+                vmin = kwargs.pop("vmin")
+            if "vmax" not in kwargs:
+                vmax = np.nanmax(kwargs["c"])
+            else:
+                vmax = kwargs.pop("vmax")
+
+            norm = Normalize(vmin = vmin, vmax = vmax)
+        elif ("c" in kwargs) & ("norm" in kwargs):
+            norm = kwargs.pop("norm")
+
+        if ("c" in kwargs):
+            if len(kwargs["c"].shape) != 2:
+                c = kwargs.pop("c")
+                if "cmap" not in kwargs:
+                    cmap = "viridis"
+                else:
+                    cmap = kwargs.pop("cmap")
+                cmapper = cm.ScalarMappable(cmap = cmap, norm = norm)
+                kwargs["c"] = cmapper.to_rgba(c)
+
+            need_colorhelp = True
+
+        else:
+            need_colorhelp = False
+
+        # Get x and y values to plot
+        x = self["LMC_B"].copy()
+        xlabel = "LMC Impact Parameter (kpc)"
+        y = self["N_{}".format(ion)].copy()
+        ylabel = r"Column Density $(cm^{-2})$"
+        if log:
+            y = np.log10(y)
+            ylabel = r"$\log_{10}(N / cm^{-2})$"
+
+
+        # Plot
+        s = ax.scatter(x, y, **kwargs)
+
+        color = s._facecolors[0]
+
+        if add_labels:
+            _ = ax.set_ylabel(ylabel, fontsize = 12)
+            _ = ax.set_xlabel(xlabel, fontsize = 12)
+
+        if not log:
+            ax.set_yscale("log")
+
+        if colorbar:
+            fig = plt.gcf()
+            cb = fig.colorbar(cmapper, **colorbar_kwargs)
+
+        # Handle upper limits
+        # Upperlimits
+        if "width" not in limit_kwargs:
+            limit_kwargs["width"] = 0.1
+        if "head_width" not in limit_kwargs:
+            limit_kwargs["head_width"] = 1
+        if "overhang" not in limit_kwargs:
+            limit_kwargs["overhang"] = .5
+
+        try:
+            default_head_length = limit_kwargs.pop("head_length")
+        except KeyError:
+            default_head_length = None
+
+
+
+
+        for ell,row in enumerate(self[self["N_{}_UPPERLIMIT".format(ion)]]):
+            x = row["LMC_B"]
+            y = row["N_{}".format(ion)].copy()
+            dy = length_factor*y
+            if log:
+                y = np.log10(y)
+                dy = length_factor/2.
+                if default_head_length is None:
+                    head_length = 0.05
+                else:
+                    head_length = default_head_length
+            else:
+                if default_head_length is None:
+                    head_length = head_length_factor*y
+                else:
+                    head_length = default_head_length
+
+            if need_colorhelp:
+                c_up = kwargs["c"][self["N_{}_UPPERLIMIT".format(ion)],:]
+                color = c_up[ell,:]
+
+            elif "color" in limit_kwargs:
+                color = limit_kwargs.pop("color")
+
+            
+
+            ax.arrow(x, y, 0, -dy, head_length = head_length, color = color, **limit_kwargs)
+
+        for ell,row in enumerate(self[self["N_{}_LOWERLIMIT".format(ion)]]):
+            x = row["LMC_B"]
+            y = row["N_{}".format(ion)].copy()
+            dy = length_factor*y
+            if log:
+                y = np.log10(y)
+                dy = length_factor/2.
+                if default_head_length is None:
+                    head_length = 0.05
+                else:
+                    head_length = default_head_length
+            else:
+                if default_head_length is None:
+                    head_length = head_length_factor*y
+                else:
+                    head_length = default_head_length
+
+            if need_colorhelp:
+                c_up = kwargs["c"][self["N_{}_LOWERLIMIT".format(ion)],:]
+                color = c_up[ell,:]
+
+            elif "color" in limit_kwargs:
+                color = limit_kwargs.pop("color")
+
+            
+
+            ax.arrow(x, y, 0, dy, head_length = head_length, color = color, **limit_kwargs)
+
+        return ax
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
